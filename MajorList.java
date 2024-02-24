0b40a946aead428eff043041b303b2c7b32d4123
isaacmayernik <mayernii@email.sc.edu>
2024-02-24 15:57:39 -0500

    finished  #12 and #17

--- MajorList.java
+++ MajorList.java
@@ -1,3 +1,28 @@
+import java.util.ArrayList;
+
 public class MajorList {
-    
+    private static ArrayList<major> majorList;
+
+    private static MajorList(){
+
+    }
+
+    public Major getInstance() {
+        Major m = new Major()
+        return m;
+    }
+
+    public Major getMajor(String major){
+        for(int i = 0; i < majorList.size(); i++){
+            if(major == majorList[i]){
+                return major;
+            }
+            return "major not found";
+        }
+    }
+
+    public void setMajor(String major){
+        Major m = new Major(major);
+        return m;
+    }
 }
