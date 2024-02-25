ac3076b0b29b456afb3652220b9c715b1eebb39f
isaacmayernik <mayernii@email.sc.edu>
2024-02-24 22:19:30 -0500

    fixed many methods. completed #44 and #45. needs stubs to be finished to test if UI java file works.

--- src/User.java
+++ src/User.java
@@ -5,6 +5,7 @@ public class User {
     private String age;
     private String email;
     private String userType;
+    private String password;
 
     public User(String userID, String firstName, String lastName, String age, String email, String userType){
         this.userID = userID;
@@ -13,5 +14,28 @@ public class User {
         this.age = age;
         this.email = email;
         this.userType = userType;
+    }
+
+    public User(String userName, String firstName, String lastName, String password){
+        this.email = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+    }
+
+    public String getUserID(){
+        return userID;
+    }
+
+    public String getUsername(){
+        return email;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getPassword(){
+        return password;
     }
 }
--- src/UserList.java
+++ src/UserList.java
@@ -21,11 +21,25 @@ public class UserList {
         return new User(userID, null, null, null, null, null);
     }
 
+    public User getUser(String userName, String password){
+        //Checks each user in users array list
+        for(User user : users){
+            if(user.getUsername().equals(userName) && user.getPassword().equals(password)){
+                return user;
+            } //checks if usernames are equals and passwords are equal
+        }
+        return null;
+    }
+
     public void setUser(){
         
     }
 
     public void editUser(String user){
 
+    }
+
+    public void addUser(User user){
+        user = new User(null, null, null, null);
     }
 }
