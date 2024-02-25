09fa75b1ed440bf8ed254e61aaa59ca59e89e151
Zechariah Fisher-Coleman <zechfc@gmail.com>
2024-02-24 22:02:51 -0500

    added a database class which stores the info from our json files

---
+++ src/Database.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Database {
+	
+	public static ArrayList<User> getUsers(){
+		return new ArrayList<User>();
+	}
+	public static ArrayList<Class> getClass(){
+		return new ArrayList<Class>();
+	}
+	public static ArrayList<Major> getMajor(){
+		return new ArrayList<Major>();
+	}
+
+}
