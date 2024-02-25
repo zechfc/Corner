d604e4e0835d38919e2fbbf6681fbfc1cc85def3
ksakers@email.sc.edu <143644689+k190123@users.noreply.github.com>
2024-02-25 10:46:53 -0500

    added #50added #50added #50added #50added #50added #50added #50added

---
+++ src/Advisor.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Advisor {
+    private ArrayList<Student> studentsSupervising;
+
+    public Advisor(String userName, String firstName, String lastName, String userID, int userRolse){
+
+    }
+
+    //Can't have overloaded methods with the same param types
+    public void getStudent(String userID){
+
+    }
+//    public void getStudent(String lastName){
+//
+//    }
+    public void addStudent(String userID){}
+
+    public void removeStudent(String userID){}
+
+    public void editAdvisorNote(String userID){}
+}
