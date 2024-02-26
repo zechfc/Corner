5b36658d89c0d5ddfe8edd0649f38a46151ff49d
isaacmayernik <mayernii@email.sc.edu>
2024-02-25 21:12:22 -0500

    fixed classlist getinstance, getSemesterPlan in Student needs work. fixed other methods to accomodate Application and UI

--- src/Advisor.java
+++ src/Advisor.java
@@ -1,29 +1,41 @@
 import java.util.ArrayList;
 import java.util.UUID;
 
-public class Advisor {
+public class Advisor extends User{
     private ArrayList<Student> studentsSupervising;
     private UUID userID;
 
 
-    public Advisor(String userName, String firstName, String lastName, String userID, int userRolse){
-
+    public Advisor(UUID userID, String firstName, String middleName, String lastName, String age, String email, String userType, String password){
+        super(userID, firstName, middleName, lastName, age, email, userType, password);
     }
 
     //Can't have overloaded methods with the same param types
     public void getStudent(String userID){
 
     }
-//    public void getStudent(String lastName){
-//
-//    }
-    public void addStudent(String userID){}
+    //public void getStudent(String lastName){}
 
-    public void removeStudent(String userID){}
+    //Adding student to system, must be admin, checked by Application.java
+    public Student addStudent(String userID){
+        return null;
+    }
+
+    public void addStudent(Student student){
+
+    }
+
+    //Removing student from system, must be admin, checked by Application.java
+    //Returns true if student removed from system (default true)
+    public boolean removeStudent(String userID){
+        UserList users = UserList.getInstance();
+        //will call users.removeStudent()
+        return true;
+    }
 
     public void editAdvisorNote(String userID){}
 
-    public ArrayList<Student> getStudentList() {
+    public ArrayList<Student> getStudentList(){
         return studentsSupervising;
     }
 }
