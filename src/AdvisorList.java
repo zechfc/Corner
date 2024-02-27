2c2d81a3dc542c98dd9f1af6d635128067a9d381
ksakers@email.sc.edu <143644689+k190123@users.noreply.github.com>
2024-02-26 21:08:39 -0500

    added #6

---
+++ src/AdvisorList.java
@@ -0,0 +1,14 @@
+public class AdvisorList {
+private  AdvisorList advisorList;
+public AdvisorList(){  //constructor
+
+}
+public AdvisorList getInstance(){ return new AdvisorList;  }
+public String getAdvisor(String userID){ return "";  }
+public String getUsername(){ return "";  }
+public String getPassword(){ return "";  }
+public void setAdvisor(){ return;  } 
+public void editAdvisor(String userID){ return;  } 
+public void addAdvisor(String userID){ return;  } 
+public boolean haveAdvisor(String email){ return false;  }
+}
