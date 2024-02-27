f7bc8fa7979d6a9ad821304a34b598c25fd281be
zechfc <zcf@email.edu.sc>
2024-02-27 12:10:44 -0500

    updated data constants

--- src/DataConstants.java
+++ src/DataConstants.java
@@ -1,33 +1,29 @@
+import java.util.ArrayList;
+
 public abstract class DataConstants {
-	protected static final String USER_FILE_NAME = "src/users.json";
     protected static final String STUDENT_FILE_NAME = "src/student.json";
     protected static final String ADVISOR_FILE_NAME = "src/advisor.json";
 
 	protected static final String USER_ID = "userid";
+    protected static final String STUDENT_LIST = "studentList";
 	protected static final String USER_USER_NAME = "email";
 	protected static final String USER_FIRST_NAME = "firstName";
     protected static final String USER_MIDDLE_NAME = "middleName";
 	protected static final String USER_LAST_NAME = "lastName";
 	protected static final String USER_AGE = "age";
 	protected static final String USER_PASSWORD = "password";
-    protected static final String USER_TYPE = "userType";
+    protected static final String ADMIN = "admin";
+    protected static final String COURSES_PAST = "coursesPast";
+    protected static final String COURSES_PRESENT = "coursesPresent";
 
-	protected static final String ADVISOR_ID = "advisorid";
-    protected static final String ADVISOR_NOTE = "advisorNote";
+
+
+    protected static final String NOTES = "notes";
 	protected static final String MAJOR = "major";
 	protected static final String CLASSIFICATION = "classification";
-    protected static final String YEAR1 = "year1";
-    protected static final String YEAR2 = "year2";
-    protected static final String YEAR3 = "year3";
-    protected static final String YEAR4 = "year4";
-    protected static final String CURRENT_YEAR = "currentYear";
     protected static final String FALL = "fall";
-    protected static final String SPRING = "spring";
-    protected static final String SUMMER = "summer";
-    protected static final String STUDENT_LIST = "studentList";
     protected static final String COURSE_CODES = "courseCodes";
-    protected static final String GRADE = "grade";
-    protected static final int TRANSFER_CREDITS = 0;
+    protected static final String TRANSFER_CREDITS = "tranferCredits";
 
     //better way to store year
 
