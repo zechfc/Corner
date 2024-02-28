4c2c84df7055d425bf74cb8ef8de23b7681468e4
ksakers@email.sc.edu <143644689+k190123@users.noreply.github.com>
2024-02-27 19:54:32 -0500

    dded #6

--- src/StudentList.java
+++ src/StudentList.java
@@ -1,14 +1,24 @@
-public class StudentList {
-private StudentList StudentList;
-public StudentList(){  //constructor
+import java.util.ArrayList;
 
-}
-public StudentList getInstance(){ return new StudentList;  }
-public String getStudent(String userID){ return "";  }
-public String getUsername(){ return "";  }
-public String getPassword(){ return "";  }
-public void setStudent(){ return;  } 
-public void editStudent(String userID){ return;  } 
-public void addStudent(String userID){ return;  } 
-public boolean haveStudent(String email){ return false;  }
+public class StudentList {
+    //private static StudentList Students;
+    private static StudentList Students;
+    private ArrayList<Student> StudentList;
+    public StudentList(){  //constructor
+        StudentList = DataLoader.getStudents();
+    }
+    public static StudentList getInstance(){
+        if (Students == null){
+            Students = new StudentList();
+        }
+        return Students;
+    }
+    public String getStudent(String userID){
+        return StudentList;  }
+    public String getUsername(){ return "";  }
+    public String getPassword(){ return "";  }
+    public void setStudent(){ return;  }
+    public void editStudent(String userID){ return;  }
+    public void addStudent(String userID){ return;  }
+    public boolean haveStudent(String email){ return false;  }
 }
