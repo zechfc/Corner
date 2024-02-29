fad5c027559f596e061036241a3b595d29b2f7fc
isaacmayernik <mayernii@email.sc.edu>
2024-02-29 11:46:08 -0500

    Forgot to update variable in CourseList

--- src/CourseList.java
+++ src/CourseList.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class CourseList {
-    private static CourseList classList;
+    private static CourseList courseList;
     private static ArrayList<Course> courses;
     
     private CourseList() {
@@ -9,17 +9,17 @@ public class CourseList {
     }
 
     public static CourseList getInstance() {
-        if (classList == null){
-            classList = new CourseList();
+        if (courseList == null){
+            courseList = new CourseList();
         }
-        return classList;
+        return courseList;
     }
 
-    public ArrayList<Course> getCourse(String className) {
+    public ArrayList<Course> getCourse(String courseName) {
         return courses;
     }
 
-    public Course checkPrereq(Course cass, double grade) {
+    public Course checkPrereq(Course course, double grade) {
         Course c = new Course(null, null, null, null, null, false, 0);
         return c;
     }
