b34ea6527c9496984f0982e83bb05a715370d2ca
ksakers@email.sc.edu <143644689+k190123@users.noreply.github.com>
2024-03-24 13:20:38 -0400

    added some functionality

--- src/CourseChoiceTest.java
+++ src/CourseChoiceTest.java
@@ -1,12 +1,57 @@
 import static org.junit.jupiter.api.Assertions.*;
-//import org.junit.AfterClass;
-//import org.junit.BeforeClass;
-import org.junit.jupiter.api.*;
 
+import org.junit.After;
+//import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+@TestInstance(Lifecycle.PER_CLASS)
 public class CourseChoiceTest {
+    private CourseChoice cc;
+    private ArrayList<Course> coursesTaken;
+    private Course prereq;
+
+        
+
+
     @BeforeAll
     public void inital_setup(){
+//        {
+//            "requireType": "OR",
+//                "courseID": [
+//            "47c4e89e-9000-4203-ba31-77441dff5028",
+//                    "1fe509c2-879d-4f76-9ad8-5f37468fcc66"
+//        ]
+//        },
+//        {
+//            "requireType": "AND",
+//                "courseID": ["0287575f-710b-4526-aa12-d87e89d9477a"]
+//        }
 
+        //checkPrerequisites
+        prereq = new Course(new ArrayList<CourseChoice>(),"47c4e89e-9000-4203-ba31-77441dff5028",
+                "ACCT404",
+                "Accounting Information Systems I",
+                "Accounting systems for business decision-making and effective control of the business enterprise.   FS: 06/01/2022.",
+                true,
+                3.0,
+                new ArrayList<>(Arrays.asList("SPRING", "SUMMER", "FALL")),
+                null,
+                "C");
+
+        // cc = new CourseChoice("3a9b678a-8dcb-4d3c-9269-3a997710fdf8", new ArrayList<String>(){{ add("3a9b678a-8dcb-4d3c-9269-3a997710fdf8"); }});
+
+        // coursesTaken.add(new Course(new ArrayList<CourseChoice>()
+        //         ,"3a9b678a-8dcb-4d3c-9269-3a997710fdf8",
+        //                 "ACCT475",
+        //                 "Integrated Business Processes with Enterprise Systems",
+        //         "The integration, configuration, and operation of accounting information within enterprise resource planning and other databases as applied to current business practices.   FS: 02/03/2021.",
+        //         true, 3.0,
+        //         new ArrayList<>(Arrays.asList("SPRING", "SUMMER", "FALL")),
+        //         null, "C"));
     }
 
     @BeforeEach
@@ -14,9 +59,21 @@ public class CourseChoiceTest {
 
     }
 
+    //checkConstructor
+    @Test
+    public void test_CourseChoiceConstructor_Valid() {
+    }
+
+    @Test
+    public void test_CourseChoiceConstructor_Invalid() {
+    }
+
     //checkPrerequisites
     @Test
     public void test_checkPrerequisites_OR_Valid(){
+        ArrayList<Course> courselist = new ArrayList<Course>();
+        courselist.add(prereq);
+        assertTrue (cc.checkPrerequisites(courselist));
 
     }
 
@@ -99,7 +156,7 @@ public class CourseChoiceTest {
     }
 
 
-    @AfterAll
+    @After
     public void teardown(){
 
     }
