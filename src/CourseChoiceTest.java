44e9837024a8d4fed1e36c6d70567e45fbe64b08
ksakers@email.sc.edu <143644689+k190123@users.noreply.github.com>
2024-03-24 11:13:26 -0400

    method outlines

--- src/CourseChoiceTest.java
+++ src/CourseChoiceTest.java
@@ -1,9 +1,106 @@
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-public class CourseChoiceTest {
+//import org.junit.AfterClass;
+//import org.junit.BeforeClass;
+import org.junit.jupiter.api.*;
 
+public class CourseChoiceTest {
+    @BeforeAll
+    public void inital_setup(){
+
+    }
+
+    @BeforeEach
+    public void setup(){
+
+    }
+
+    //checkPrerequisites
+    @Test
+    public void test_checkPrerequisites_OR_Valid(){
+
+    }
+
+    @Test
+    public void test_checkPrerequisites_OR_Invalid(){
+
+    }
+    @Test
+    public void test_checkPrerequisites_AND_Valid(){
+
+    }
+
+    @Test
+    public void test_checkPrerequisites_AND_Invalid(){
+
+    }
+
+    @Test
+    public void test_checkPrerequisites_PRE_OR_COREQ_Valid(){
+
+    }
+
+    @Test
+    public void test_checkPrerequisites_PRE_OR_COREQ_Invalid(){
+
+    }
+
+
+    //checkCoreqs
+    @Test
+    public void test_checkCoreqs_Valid(){
+
+    }
+
+    @Test
+    public void test_checkCoreqs_Invalid(){
+
+    }
+
+
+    //getCoursesToString
+    @Test
+    public void test_getCoursesToString_OR_Valid(){
+
+    }
+
+    @Test
+    public void test_getCoursesToString_OR_Invalid(){
+
+    }
+
+    @Test
+    public void test_getCoursesToString_AND_Valid(){
+
+    }
+
+    @Test
+    public void test_getCoursesToString_AND_Invalid(){
+
+    }
+
+    @Test
+    public void test_getCoursesToString_PRE_OR_COREQ_Valid(){
+
+    }
+
+    @Test
+    public void test_getCoursesToString_PRE_OR_COREQ_Invalid(){
+
+    }
+
+    @Test
+    public void test_getCoursesToString_COREQ_Valid(){
+
+    }
+
+    @Test
+    public void test_getCoursesToString_COREQ_Invalid(){
+
+    }
+
+
+    @AfterAll
+    public void teardown(){
+
+    }
 }
