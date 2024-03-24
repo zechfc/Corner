3b8006a81828dcacb247de5cb8eccc38aa87a834
ksakers@email.sc.edu <143644689+k190123@users.noreply.github.com>
2024-03-24 09:57:52 -0400

    choicetest

---
+++ src/CourseChoiceTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class CourseChoiceTest {
+
+}
